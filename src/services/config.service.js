b35862d985b28567871a17742c88ff198456272f
MundanePain <63653543+MundanePain@users.noreply.github.com>
2023-08-30 15:00:44 +0100

    Create config.service.js

---
+++ src/services/config.service.js
@@ -0,0 +1,1 @@
+angular.module('app').factory('ConfigService', [function () { var service = {}; service.apiBase = 'https://quizapism-env-1.eba-imztifqx.eu-west-2.elasticbeanstalk.com/'; return service; }]);
