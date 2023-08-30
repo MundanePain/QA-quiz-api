55fdba74e7f003f676b7056aed0f22ff807564ba
MundanePain <63653543+MundanePain@users.noreply.github.com>
2023-08-30 15:14:39 +0100

    Update config.service.js

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,1 +1,4 @@
-angular.module('app').factory('ConfigService', [function () { var service = {}; service.apiBase = 'https://quizapism-env-1.eba-imztifqx.eu-west-2.elasticbeanstalk.com/'; return service; }]);
+angular.module('app').factory('ConfigService', [function () {
+  var service = {}; 
+  service.apiBase = 'https://quizapism-env-1.eba-imztifqx.eu-west-2.elasticbeanstalk.com/';
+  return service; }]);
